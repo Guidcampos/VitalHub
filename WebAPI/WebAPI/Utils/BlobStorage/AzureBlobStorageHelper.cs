1200b9c7fab1250c249d296eccb05c38f0b5f2ab
Guilherme De Campos Ribeiro <guilherme.campos.r@gmail.com>
2024-04-19 16:30:13 -0300

    API NOVA

---
+++ WebAPI/WebAPI/Utils/BlobStorage/AzureBlobStorageHelper.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Storage.Blobs;
+
+namespace WebAPI.Utils.BlobStorage
+{
+    public static class AzureBlobStorageHelper
+    {
+        public static async Task<string> UploadImageBlobAsync(IFormFile arquivo, string stringConexao, string nomeContainer)
+        {
+            try
+            {
+                //VERIFICA SE EXISTE UM ARQUIVO
+                if (arquivo != null)
+                {
+                    //GERA UM NOME UNICO + EXTENSAO DO ARQUIVO
+                    var blobName = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(arquivo.FileName);
+
+                    //CRIAR UMA INSTANCIA DO CLIENT BLOBSERVICE E PASSA A STRING DE CONEXAO
+                    var blobServiceClient = new BlobServiceClient(stringConexao);
+
+                    //OBTEM UM CONTAINER CLIENT USANDO O NOME DO CONTAINER DO BLOB
+                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(nomeContainer);
+
+                    //OBTEM UM BLOB CLIENT USANDO O NAME
+                    var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                    //ABRE O FLUXO DE ENTRADA DO ARQUIVO(FOTO)
+                    using (var stream = arquivo.OpenReadStream())
+                    {
+                        //CARREGA O ARQUIVO(FOTO) PARA O BLOB STORAGE DE FORMA ASSINCRONA 
+                        await blobClient.UploadAsync(stream, true);
+                    }
+                    //RETORNA A URI DO BLOB COMO STRING
+                    return blobClient.Uri.ToString();
+                }
+                else
+                {
+                    //imagem padrao
+                    return "https://blobvitalhubg13campos.blob.core.windows.net/containervitalhub/profilepattern.png";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Utils/Ocr/OcrService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace WebAPI.Utils.Ocr
+{
+    public class OcrService
+    {
+        //colocar key do computer vision do portal azure
+        private readonly string _subscriptKey = "bea890619294458ca8a572a68afdf551";
+
+        private readonly string _endpoint = "https://cvvitalhubcampos.cognitiveservices.azure.com/";
+
+        public async Task<string> RecognizeTextAsync(Stream imageStream)
+        {
+            try
+            {
+                var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(_subscriptKey))
+                {
+                    Endpoint = _endpoint
+                };
+
+                var ocrResult = await client.RecognizePrintedTextInStreamAsync(true, imageStream);
+
+                return ProcessRecognitionResult(ocrResult);
+            }
+            catch (Exception ex)
+            {
+                return "Erro ao reconhecer o texto :" + ex.Message;
+            }
+        }
+
+        private static string ProcessRecognitionResult(OcrResult result)
+        {
+            try
+            {
+                string recognizedText = "";
+
+                foreach (var region in result.Regions) 
+                {
+                    foreach(var line in region.Lines) 
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            recognizedText += word.Text + " ";
+                        }
+
+                        recognizedText += "\n";
+                    }
+                }
+
+                return recognizedText;
+
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
